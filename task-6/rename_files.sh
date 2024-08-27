ae96fd608b979047231d9d1965c81c25f9da4e4a
khaled <KhaledSA15Me@gmail.com>
2024-08-27 17:40:47 +0200

    Handled an error occurs when the sent directory has sub-directories contain .txt files

--- task-6/rename_files.sh
+++ task-6/rename_files.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+text_files_list=$(ls $1 | grep -Eioh "([[:alnum:]_.-]+?\.txt)") 
+
+for i in $text_files_list
+do 
+	if [[ -f $1/$i ]]
+		then		
+			mv "$1/$i" "$1/old_$i"
+	fi
+done
+
+
+# ================================
+exit
+# ================================
+
+# this was the former script 
+# it faces a problem when the sent directory has sub-directories which contain ".txt" filess
+
 text_files_list=$(find $1 -name "*.txt" -type f | grep -Eioh "([[:alnum:]_.-]+?\.txt)")
 
 for i in $text_files_list
