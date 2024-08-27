123d131c62fa26e6f10c5c9a5811284bb3d95046
khaled <KhaledSA15Me@gmail.com>
2024-08-27 09:59:43 +0200

    Fix rename_files.sh script to rename only text files

--- task-6/rename_files.sh
+++ task-6/rename_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in $(ls $1)
+text_files_list=$(find $1 -name "*.txt" -type f | grep -Eioh "([[:alnum:]_.-]+?\.txt)")
+
+for i in $text_files_list
 do 
 	mv "$1/$i" "$1/old_$i"
 done
